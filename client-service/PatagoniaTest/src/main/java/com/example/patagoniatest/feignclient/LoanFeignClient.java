2389e2d9a8f5a13173bcdfa4683b616857fce7fc
AlejBravo <marialebravor@gmail.com>
2022-06-26 16:15:47 -0300

    loan service

---
+++ client-service/PatagoniaTest/src/main/java/com/example/patagoniatest/feignclient/LoanFeignClient.java
@@ -0,0 +1,15 @@
+package com.example.patagoniatest.feignclient;
+
+import com.example.patagoniatest.entity.Loan;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@FeignClient(name = "loan-service")
+@RequestMapping("/loan")
+public interface LoanFeignClient {
+
+    @PostMapping()
+    Loan saveLoan(@RequestBody Loan loan);
+}
---
+++ client-service/PatagoniaTest/src/main/java/com/example/patagoniatest/service/ClientService.java
@@ -0,0 +1,77 @@
+package com.example.patagoniatest.service;
+
+import com.example.patagoniatest.entity.Loan;
+import com.example.patagoniatest.feignclient.LoanFeignClient;
+import com.example.patagoniatest.model.Client;
+import com.example.patagoniatest.repository.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.OptionalDouble;
+import java.util.stream.Collectors;
+
+@Service
+public class ClientService {
+
+    private final ClientRepository clientRepository;
+
+    @Autowired
+    public ClientService(ClientRepository clientRepository) {
+        this.clientRepository = clientRepository;
+    }
+
+    @Autowired
+    LoanFeignClient loanFeignClient;
+
+    public List<Client> getClients() {
+        return clientRepository.findAll();
+    }
+
+    public Client addClient(Client client) {
+        return clientRepository.save(client);
+    }
+
+    public void deleteClient(Long id) {
+        clientRepository.deleteById(id);
+    }
+
+    public Optional<Client> getClient(Long id) {
+        return clientRepository.findById(id);
+    }
+
+    @Transactional
+    public void updateClient(Client client, Long id) {
+        Client updatedClient = clientRepository.findById(id).orElseThrow(() -> new IllegalStateException(
+                "client with id: " + id + "doesn´t exists"
+        ));
+        if (!Objects.equals(updatedClient.getFullName(), client.getFullName())){
+            updatedClient.setFullName(client.getFullName());
+        }
+        if (updatedClient.getIncome() != client.getIncome()){
+            updatedClient.setIncome(client.getIncome());
+        }
+    }
+
+    public OptionalDouble getEarningsAverage() {
+        List<Client> clients = clientRepository.findAll();
+        return clients.stream()
+                .mapToDouble(Client::getIncome)
+                .average();
+    }
+
+    public List<Client> getTopEarners() {
+        List<Client> clients = clientRepository.findAll();
+        return clients.stream()
+                .filter(client -> client.getIncome() >= 10000).collect(Collectors.toList());
+    }
+
+    public Loan saveLoan(Long clientId, Loan loan){
+        loan.setClientId(clientId);
+        Loan loanNew = loanFeignClient.saveLoan(loan);
+        return loanNew;
+    }
+}
